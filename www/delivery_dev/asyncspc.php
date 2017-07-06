3b52d219387b04772b10d4c282839873584809a7
Kuteiko <kuteiko@mail.ru>
2017-07-06 09:54:58 +0300

    #5 Get banners campaign and keywords

--- www/delivery_dev/asyncspc.php
+++ www/delivery_dev/asyncspc.php
@@ -1,91 +1,109 @@
 <?php
 
-/*
-+---------------------------------------------------------------------------+
-| Revive Adserver                                                           |
-| http://www.revive-adserver.com                                            |
-|                                                                           |
-| Copyright: See the COPYRIGHT.txt file.                                    |
-| License: GPLv2 or later, see the LICENSE.txt file.                        |
-+---------------------------------------------------------------------------+
-*/
+	/*
+	+---------------------------------------------------------------------------+
+	| Revive Adserver                                                           |
+	| http://www.revive-adserver.com                                            |
+	|                                                                           |
+	| Copyright: See the COPYRIGHT.txt file.                                    |
+	| License: GPLv2 or later, see the LICENSE.txt file.                        |
+	+---------------------------------------------------------------------------+
+	*/
 
 // Require the initialisation file
-require_once '../../init-delivery.php';
+	require_once '../../init-delivery.php';
 
 // Required files
-require_once MAX_PATH . '/lib/max/Delivery/adSelect.php';
-require_once MAX_PATH . '/lib/max/Delivery/javascript.php';
+	require_once MAX_PATH . '/lib/max/Delivery/adSelect.php';
+	require_once MAX_PATH . '/lib/max/Delivery/javascript.php';
 
-MAX_commonSetNoCacheHeaders();
+	MAX_commonSetNoCacheHeaders();
 
-/*-------------------------------------------------------*/
-/* Register input variables                              */
-/*-------------------------------------------------------*/
+	/*-------------------------------------------------------*/
+	/* Register input variables                              */
+	/*-------------------------------------------------------*/
 
-MAX_commonRegisterGlobalsArray(array('zones' ,'source', 'block', 'blockcampaign', 'exclude', 'q', 'prefix'));
+	MAX_commonRegisterGlobalsArray([ 'zones', 'campaign', 'keywords', 'source', 'block', 'blockcampaign', 'exclude', 'q', 'prefix' ]);
 
-/*-------------------------------------------------------*/
-/* Main code                                             */
-/*-------------------------------------------------------*/
+	/*-------------------------------------------------------*/
+	/* Main code                                             */
+	/*-------------------------------------------------------*/
 
 // Protect from Reflected File Download attacks
-if (preg_match('/[^a-zA-Z0-9_-]/', $prefix)) {
-    MAX_sendStatusCode(400);
-    exit;
-}
+	if (preg_match('/[^a-zA-Z0-9_-]/', $prefix))
+	{
+		MAX_sendStatusCode(400);
+		exit;
+	}
+
+	if (preg_match('/[^0-9_-]/', $zones))
+	{
+		$zones = MAX_cacheGetZoneIdByName($zones);
+	}
+	if (preg_match('/[^0-9_-]/', $campaign))
+	{
+		$campaign = MAX_cacheGetCampaignIdByName($campaign);
+	}
 
 // Derive the source parameter
-$source = MAX_commonDeriveSource($source);
+	$source = MAX_commonDeriveSource($source);
 
-$spc_output = array();
+	$spc_output = [];
 
-if(!empty($zones)) {
-    $zones = explode('|', $zones);
-    foreach ($zones as $id => $thisZoneid) {
-        $zonename = $prefix.$id;
+	if (!empty($zones))
+	{
+		$zones = explode('|', $zones);
+		foreach ($zones as $id => $thisZoneid)
+		{
+			$zonename = $prefix . $id;
 
-        // Clear deiveryData between iterations
-        unset($GLOBALS['_MAX']['deliveryData']);
+			// Clear deiveryData between iterations
+			unset($GLOBALS[ '_MAX' ][ 'deliveryData' ]);
 
-        $what = 'zone:'.$thisZoneid;
+			$what = 'zone:' . $thisZoneid;
 
-        // Get the banner
-        $output = MAX_adSelect($what, $clientid, $target, $source, $withtext, $charset, $context, true, $ct0, $GLOBALS['loc'], $GLOBALS['referer']);
+			// Get the banner
+			$output = MAX_adSelect($what, $campaign, $target, $source, $withtext, $charset, $context, true, $ct0, $GLOBALS[ 'loc' ], $GLOBALS[ 'referer' ], $keywords);
 
-        $spc_output[$zonename] = array(
-            'html' => $output['html'],
-            'width' => isset($output['width']) ? $output['width'] : 0,
-            'height' => isset($output['height']) ? $output['height'] : 0,
-            'iframeFriendly' => isset($output['iframeFriendly']) ? $output['iframeFriendly'] : false,
-        );
+			$spc_output[ $zonename ] = [
+				'html'           => $output[ 'html' ],
+				'width'          => isset($output[ 'width' ]) ? $output[ 'width' ] : 0,
+				'height'         => isset($output[ 'height' ]) ? $output[ 'height' ] : 0,
+				'iframeFriendly' => isset($output[ 'iframeFriendly' ]) ? $output[ 'iframeFriendly' ] : false,
+			];
 
-        // Block this banner for next invocation
-        if (!empty($block) && !empty($output['bannerid'])) {
-            $output['context'][] = array('!=' => 'bannerid:' . $output['bannerid']);
-        }
-        // Block this campaign for next invocation
-        if (!empty($blockcampaign) && !empty($output['campaignid'])) {
-            $output['context'][] = array('!=' => 'campaignid:' . $output['campaignid']);
-        }
-        // Pass the context array back to the next call, have to iterate over elements to prevent duplication
-        if (!empty($output['context'])) {
-            foreach ($output['context'] as $id => $contextArray) {
-                if (!in_array($contextArray, $context)) {
-                    $context[] = $contextArray;
-                }
-            }
-        }
-    }
-}
+			// Block this banner for next invocation
+			if (!empty($block) && !empty($output[ 'bannerid' ]))
+			{
+				$output[ 'context' ][] = [ '!=' => 'bannerid:' . $output[ 'bannerid' ] ];
+			}
+			// Block this campaign for next invocation
+			if (!empty($blockcampaign) && !empty($output[ 'campaignid' ]))
+			{
+				$output[ 'context' ][] = [ '!=' => 'campaignid:' . $output[ 'campaignid' ] ];
+			}
+			// Pass the context array back to the next call, have to iterate over elements to prevent duplication
+			if (!empty($output[ 'context' ]))
+			{
+				foreach ($output[ 'context' ] as $id => $contextArray)
+				{
+					if (!in_array($contextArray, $context))
+					{
+						$context[] = $contextArray;
+					}
+				}
+			}
+		}
+	}
 
-MAX_cookieFlush();
+	MAX_cookieFlush();
 
-if (!empty($_SERVER['HTTP_ORIGIN']) && preg_match('#https?://#', $_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
-    header("Access-Control-Allow-Credentials: true");
-}
+	if (!empty($_SERVER[ 'HTTP_ORIGIN' ]) && preg_match('#https?://#', $_SERVER[ 'HTTP_ORIGIN' ]))
+	{
+		header("Access-Control-Allow-Origin: " . $_SERVER[ 'HTTP_ORIGIN' ]);
+		header("Access-Control-Allow-Credentials: true");
+	}
 
-header("Content-Type: application/json");
+	header("Content-Type: application/json");
 
-echo json_encode($spc_output);
+	echo json_encode($spc_output);
